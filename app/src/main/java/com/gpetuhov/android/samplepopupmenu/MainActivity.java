54988ed3c88ecd4ea0a69d1b40316b63e92a9345
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-05-21 11:02:18 +0300

    Add popup menu

--- app/src/main/java/com/gpetuhov/android/samplepopupmenu/MainActivity.java
+++ app/src/main/java/com/gpetuhov/android/samplepopupmenu/MainActivity.java
@@ -2,6 +2,10 @@ package com.gpetuhov.android.samplepopupmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button button = findViewById(R.id.button);
+    button.setOnClickListener(v -> {
+      PopupMenu popupMenu = new PopupMenu(MainActivity.this, button);
+      popupMenu.setOnMenuItemClickListener(item -> {
+        int itemId = item.getItemId();
+
+        return true;
+
+      });
+
+      MenuInflater inflater = popupMenu.getMenuInflater();
+      inflater.inflate(R.menu.popup_menu, popupMenu.getMenu());
+      popupMenu.show();
+    });
   }
 }
